c0d4c4cf76242f5933e8a7ecc382c97606f7d43a
Akash Kriplani <aakash23kriplani@gmail.com>
2022-12-03 20:22:36 +0530

    Add a POST request to add a new book to the books list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
@@ -23,8 +24,12 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [...books, { id: Math.round(Math.random() * 9999), title }];
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title: title
+    });
+
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
